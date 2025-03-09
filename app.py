d92ab1a12d59007afd380b953ca427b4b646226a
Nguyễn Tài Quân <taiquan03a@gmail.com>
2025-03-09 14:49:40 +0700

    test jenkins

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api/greet', methods=['GET'])
+def greet():
+    name = request.args.get('name', 'Guest')
+    message = f'Hello, {name}!'
+    return render_template('greet.html', message=message)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
